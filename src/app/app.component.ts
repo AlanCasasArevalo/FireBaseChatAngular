3c59235bfaf24bbad7505cb689c6e47745bcfdea
Alan Casas <alancasasarevalo@gmail.com>
2018-04-18 19:47:55 +0200

    firebase and angularfire added, bootstrap added to index, style.css added, dummy messages from firebase to check if works, imports to app.component.ts, imports to app.module, environment configuration done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
 
+import { Observable } from 'rxjs/Observable';
+
+// angular/angularfire2
+import { AngularFirestore } from 'angularfire2/firestore';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+
+  messages: Observable<any[]>;
+
+  constructor(database: AngularFirestore) {
+    this.messages = database.collection('messages').valueChanges();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,25 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+// Library (see attached: https://github.com/angular/angularfire2/blob/master/docs/install-and-setup.md)
+import { AngularFireModule } from 'angularfire2';
+import { AngularFirestoreModule } from 'angularfire2/firestore';
+import { AngularFireStorageModule } from 'angularfire2/storage';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+
+import { environment } from '../environments/environment';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFirestoreModule, // imports firebase/firestore, only needed for database features
+    AngularFireAuthModule, // imports firebase/auth, only needed for auth features,
+    AngularFireStorageModule // imports firebase/storage only needed for storage features
   ],
   providers: [],
   bootstrap: [AppComponent]
